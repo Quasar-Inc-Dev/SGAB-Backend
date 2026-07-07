36632fbb0fbc115564f24569c126ba14aeacc913
Martins <166885227+HeberMartins@users.noreply.github.com>
2026-07-07 20:27:55 -0300

    Estruturação do projeto

---
+++ src/test/java/sgab/sgab/SgabApplicationTests.java
@@ -0,0 +1,13 @@
+package sgab.sgab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
